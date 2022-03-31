4693b58c822aa144fe0d115f6fc7351d9d57779c
MaksHaks <girin.ma@yandex.ru>
2022-03-31 20:45:58 +0300

    fix

--- src/Parser.php
+++ src/Parser.php
@@ -9,7 +9,7 @@ use Symfony\Component\Yaml\Yaml;
 function parse(string $path): array
 {
     if (substr($path, -4) === "json") {
-        $fileContent = json_decode(file_get_contents($path), true);
+        $fileContent = json_decode(file_get_contents($path), true, 512, JSON_THROW_ON_ERROR);
         return $fileContent;
     } else {
         $fileContent = Yaml::parseFile($path);
